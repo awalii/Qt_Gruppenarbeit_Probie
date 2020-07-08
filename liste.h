800e929cd6cbc97bea4186c5d4ca41845f5b430d
rcooleo <62471403+rcooleo@users.noreply.github.com>
2020-07-08 16:28:14 +0200

    liste+file

--- liste.h
+++ liste.h
@@ -2,6 +2,7 @@
 #define LISTE_H
 
 #include <QDialog>
+#include "person.h"
 
 namespace Ui {
 class Liste;
@@ -15,13 +16,19 @@ public:
     explicit Liste(QWidget *parent = nullptr);
     ~Liste();
 
+    void setreihe(){this->reihe+=1;}
+
 private slots:
     void on_btn_laden_clicked();
 
     void on_btn_exit2_clicked();
 
+    void on_btn_hin_clicked();
+
 private:
     Ui::Liste *ui;
+    int reihe=1;
+    class Person *Personen[10];
 };
 
 #endif // LISTE_H
