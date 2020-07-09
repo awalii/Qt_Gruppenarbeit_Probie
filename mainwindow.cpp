613c830a927ce72c4b05cb983309d7ae0fd03a94
rcooleo <62471403+rcooleo@users.noreply.github.com>
2020-07-09 15:30:36 +0200

    login

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->ui->lbl_easter->hide();
+    this->ui->btn_reset->hide();
+    this->ui->lbl_combi->hide();
 }
 
 MainWindow::~MainWindow()
@@ -17,11 +20,18 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_btn_exit_clicked()
 {
-    close();
+    if(egg==2)
+    {
+        this->ui->lbl_combi->hide();
+        this->ui->lbl_easter->show();
+        this->ui->btn_reset->show();
+    }
+    else close();
 }
 
 void MainWindow::on_btn_clear_clicked()
 {
+    if(egg==1)egg+=1;
     this->ui->lbl_error->clear();
     this->ui->line_name->clear();
     this->ui->line_pass->clear();
@@ -44,9 +54,10 @@ void MainWindow::on_btn_login_clicked()
     }else
 
     {
-        this->ui->lbl_error->setText("Sie haben Falsche UserName oder Password eingegeben");
-        this->ui->lbl_error->setStyleSheet("color: #c8010b ; font-style:bold");
 
+        this->ui->lbl_error->setText("EINGABE FALSCH!");
+        this->ui->lbl_error->setStyleSheet("color: #c8010b ; font-style:bold");
+        egg=1;
     }
 }
 
@@ -57,8 +68,20 @@ void MainWindow::on_rd_btn_clicked(bool checked)
 
     if(checked == 1){
         this->ui->line_pass->setEchoMode(QLineEdit::Normal);
+        this->ui->lbl_combi->show();
+        this->ui->lbl_combi->setText("LEFT,RIGHT,MIDDLE");
     }
     else{
         this->ui->line_pass->setEchoMode(QLineEdit::Password);
+        this->ui->lbl_combi->clear();
+        this->ui->lbl_combi->hide();
     }
+}
+
+
+void MainWindow::on_btn_reset_clicked()
+{
+    this->ui->lbl_easter->hide();
+    this->ui->btn_reset->hide();
+    this->egg=0;
 }
